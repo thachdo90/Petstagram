c2a872dfd0dbc0979444687e6dcc6c16c91cd41f
Thach Do <thachdo90@gmail.com>
2022-11-05 17:38:55 -0700

    created a single handle change handler to handle changes to any of the form fields

--- App.js
+++ App.js
@@ -3,19 +3,35 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView, Button, TextInput } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('');
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+    name: '',
+    dob: '',
+    breed: '',
+    toy: ''
+  });
+
   const handleNameChange = (text) => {
     setName(text);
+  }
+  const handleChange = (id, text) => {
+    setFormValues({...formValues, [id]: text});
   }
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <FormField label="Pet's name" value={name} onChange={handleNameChange} placeholder='name'/>
+      <FormField id='email' label='Email:' value={formValues.email} onChange={handleChange} placeholder='email'/>
+      <FormField id='password' label='Password:' value={formValues.password} onChange={handleChange} placeholder='password'/>
+      <FormField id='name' label="Pet's name:" value={formValues.name} onChange={handleChange} placeholder='name'/>
+      <FormField id='dob' label='DOB:' value={formValues.dob} onChange={handleChange} placeholder='Month Day, Year'/>
+      <FormField id='breed' label='Breed:' value={formValues.breed} onChange={handleChange} placeholder='breed'/>
+      <FormField id='toy' label='Favorite Toy:' value={formValues.toy} onChange={handleChange} placeholder='toy'/>
     </View>
   );
 }
 
-export const FormField = ({ label, value, onChange, placeholder}) => (
+export const FormField = ({ id, label, value, onChange, placeholder}) => (
   <View>
     <Text>
       {label}
@@ -24,7 +40,7 @@ export const FormField = ({ label, value, onChange, placeholder}) => (
       {value}
     </Text>
     <TextInput
-      onChangeText={text => onChange(text)}
+      onChangeText={text => onChange(id, text)}
       placeholder={placeholder}>
     </TextInput>
   </View>
